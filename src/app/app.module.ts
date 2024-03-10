2379aec28788fdfa664006d2f36bd48a907c36f8
tanupria07 <tanupria07>
2024-03-10 22:49:57 +0530

    Need to add filter functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxGridModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [
@@ -10,11 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    IgxGridModule
   ],
   providers: [
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/localData.ts
@@ -0,0 +1,32 @@
+export const localData = [
+    {
+        id: 1,
+        username: 'john_doe',
+        name: 'John Doe',
+        occupation: 'Software Engineer'
+    },
+    {
+        id: 2,
+        username: 'jane_smith',
+        name: 'Jane Smith',
+        occupation: 'Data Scientist'
+    },
+    {
+        id: 3,
+        username: 'alex_wong',
+        name: 'Alex Wong',
+        occupation: 'UX Designer'
+    },
+    {
+        id: 4,
+        username: 'sara_jackson',
+        name: 'Sara Jackson',
+        occupation: 'Product Manager'
+    },
+    {
+        id: 5,
+        username: 'michael_brown',
+        name: 'Michael Brown',
+        occupation: 'Financial Analyst'
+    }
+];
